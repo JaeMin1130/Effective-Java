b7f4777292bca7000228a92de018df2b00f38256
JaeMin1130 <ija06598@gmail.com>
2024-06-19 10:32:00 +0900

    아이템 18 : 상속보다는 컴포지션을 사용하라

--- src/chapter4/item18/InstrumentedHashSet.java
+++ src/chapter4/item18/InstrumentedHashSet.java
@@ -1,4 +1,5 @@
 package chapter4.item18;
+
 import java.util.*;
 
 // 코드 18-1 잘못된 예 - 상속을 잘못 사용했다.
@@ -13,12 +14,14 @@ public class InstrumentedHashSet<E> extends HashSet<E> {
         super(initCap, loadFactor);
     }
 
-    @Override public boolean add(E e) {
+    @Override
+    public boolean add(E e) {
         addCount++;
         return super.add(e);
     }
 
-    @Override public boolean addAll(Collection<? extends E> c) {
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
         addCount += c.size();
         return super.addAll(c);
     }
@@ -32,6 +35,6 @@ public class InstrumentedHashSet<E> extends HashSet<E> {
         s.addAll(List.of("틱", "탁탁", "펑"));
 
         // HashSet의 addAll()이 add()를 호출하기 때문에, 의도하지 않은 addCount++이 실행된다(3이 아닌 6이 출력된다).
-        System.out.println(s.getAddCount()); 
+        System.out.println(s.getAddCount());
     }
 }
